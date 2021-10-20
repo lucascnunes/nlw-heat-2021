7f899cf58a0c373c7cbf7b32bbeb821dec4398f5
Lucas Nunes <lucas.nunes.chizukovr@gmail.com>
2021-10-19 22:04:43 -0300

    nlw heat

---
+++ node/src/routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLastMessagesController } from "./controllers/GetLastMessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
+
+router.get("/messages/last", new GetLastMessagesController().handle);
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
+
+export { router }
---
+++ react/src/App.tsx
@@ -0,0 +1,9 @@
+import styles from './App.modules.scss';
+
+export function App() {
+  return (
+    <main className={styles.contentWrapper}>
+      <h1>Hello World</h1>
+    </main>
+  )
+}
